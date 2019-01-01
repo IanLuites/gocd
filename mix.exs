e3c361a7599950dcfd5f5d293f6261b20979fef6
Ian Luites <IanLuites@users.noreply.github.com>
2019-01-01 15:18:15 +0100

    Add project settings.

---
+++ mix.exs
@@ -0,0 +1,71 @@
+defmodule GoCD.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :gocd,
+      version: "0.0.1",
+      description: "A GoCD client library for Elixir.",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+
+      # Testing
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [ignore_warnings: ".dialyzer", plt_add_deps: true],
+
+      # Docs
+      name: "CommonX",
+      source_url: "https://github.com/IanLuites/gocd",
+      homepage_url: "https://github.com/IanLuites/gocd",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ]
+    ]
+  end
+
+  def package do
+    [
+      name: :gocd,
+      maintainers: ["Ian Luites"],
+      licenses: ["MIT"],
+      files: [
+        # Elixir
+        "lib/gocd",
+        "lib/gocd.ex",
+        ".formatter.exs",
+        "mix.exs",
+        "README*",
+        "LICENSE*"
+      ],
+      links: %{
+        "GitHub" => "https://github.com/IanLuites/gocd"
+      }
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:common_x, "~> 0.0.1"},
+      {:httpx, "~> 0.0.12"},
+
+      # Dev Only
+      {:analyze, "~> 0.1.3", optional: true, runtime: false, only: [:dev, :test]},
+      {:dialyxir, "~> 1.0.0-rc.4", optional: true, runtime: false, only: [:dev, :test]}
+    ]
+  end
+end
