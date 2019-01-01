c22f3a5896032f5e621ca817b36fa851ff3e12b7
Ian Luites <IanLuites@users.noreply.github.com>
2019-01-01 15:18:00 +0100

    Add project config.

---
+++ config/config.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+# config :gocd, Example,
+#   url: System.get_env("GOCD_URL"),
+#   ciphers: [
+#     aes: System.get_env("GOCD_AES_CIPHER"),
+#     des: System.get_env("GOCD_DES_CIPHER")
+#   ],
